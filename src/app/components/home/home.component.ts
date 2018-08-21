2c8e5e05f0fb02ed20058736619d248dc73fdb6d
acerbastimur <umutacerbastimur@gmail.com>
2018-08-21 10:56:24 +0300

    tweenmax is added but types

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as firebase from 'firebase/app';
+
+import {Power1, Bounce} from 'gsap/all';
+
+declare var TweenMax: any;
 
 @Component({
   selector: 'app-home',
@@ -8,6 +12,7 @@ import * as firebase from 'firebase/app';
 
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('mushroom') box: ElementRef;
 
   constructor() {
     firebase.database().ref().child('test').on('value', data => {
@@ -16,7 +21,13 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
-  }
+    this.doIt();
 
+  }
+  doIt(): void {
+
+    TweenMax.fromTo(this.box.nativeElement, 2, {x: 20}, {x: 440, ease: Power1.easeOut});
+    TweenMax.fromTo(this.box.nativeElement, 2, {y: 20}, {y: 440, ease: Bounce.easeOut});
+  }
 }
 
